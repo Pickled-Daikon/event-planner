b2546c16e08fac36879640b1fa3b36d65390c759
jlorias <jlorias@hawaii.edu>
2020-02-28 10:24:44 -1000

    Added function to make .ics file

--- src/api/createics.js
+++ src/api/createics.js
@@ -16,5 +16,36 @@ ics.createEvent(example, (error, value) => {
   if (error) {
     console.log(error);
   }
-  console.log(value);
+  return value;
 });
+
+/**
+ *
+ * @param {{
+ *   name: String,
+ *   description: String,
+ *   startDateTime: String,
+ *   endDateTime: String
+ * }} eventObject
+ */
+function createICS(eventObject) {
+  const { name, description, startDateTime, endDateTime } = eventObject;
+  const start = new Date(startDateTime);
+  const end = new Date(endDateTime);
+  let result = '';
+
+  const icsFile = {
+    // eslint-disable-next-line max-len
+    start: [start.getFullYear(), (start.getMonth() + 1), start.getDate(), start.getHours(), start.getMinutes()],
+    end: [end.getFullYear(), (end.getMonth() + 1), end.getDate(), end.getHours(), end.getMinutes()],
+    title: name,
+    description,
+  };
+  ics.createEvent(icsFile, (error, value) => {
+    if (error) {
+      console.log(error);
+    }
+    result = value;
+  });
+  return result;
+}
