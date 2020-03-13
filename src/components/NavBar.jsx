e80670798da41931c8e226bbb7af0a97c5a58a1f
William Post <wpost@hawaii.edu>
2020-03-12 20:11:09 -1000

    added dumb logout btn

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Header, Icon } from 'semantic-ui-react';
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
+import {storeJwtToken} from "../api/users";
+import { Header, Icon, Button } from 'semantic-ui-react';
 import '../style.css';
 
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleLogout = () => {
+    storeJwtToken(null);
+    setIsLoggedIn(false);
+  };
+
+  if (!isLoggedIn) {
+    return <Redirect to={'../'} />
+  }
+
   return (
     <div className="ui borderless topmenu menu">
       <Header as="h3" className="content">
         <Icon name="calendar" size="large" />
         Daikon Calendar
       </Header>
+      <Button floated={'right'} onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
   );
 }
