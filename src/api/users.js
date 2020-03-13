b6d28b8933e9f12192c8e087a21f63009c18bca2
William Post <wpost@hawaii.edu>
2020-03-12 18:51:56 -1000

    added client api calls for users

---
+++ src/api/users.js
@@ -0,0 +1,106 @@
+import { API_ROOT_URL } from './constants';
+
+export const LOGIN_URL = `${API_ROOT_URL}/users/login`;
+export const CREATE_USER_URL = `${API_ROOT_URL}/users/create`;
+export const VERIFY_TOKEN_URL = `${API_ROOT_URL}/users/verify`;
+
+export const JWT_TOKEN = 'jwtToken';
+
+function storeJwtToken(token) {
+  localStorage.setItem(JWT_TOKEN, token);
+}
+
+function getJwtToken() {
+  return localStorage.getItem(JWT_TOKEN);
+}
+
+async function login(email, password) {
+  let jsonResp;
+  try {
+    const resp = await await fetch(LOGIN_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: {
+        email,
+        password,
+      },
+    });
+    jsonResp = await resp.json();
+  } catch (e) {
+    throw new Error('Failed to login');
+  }
+
+  if (jsonResp.error) {
+    throw new Error('Failed to login');
+  }
+
+  if (!jsonResp.jwtToken) {
+    throw new Error('Failed to login');
+  }
+  return jsonResp.jwtToken;
+}
+
+async function createUser(userObj) {
+  let jsonResp;
+  try {
+    const resp = await fetch(CREATE_USER_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: userObj,
+    });
+    jsonResp = await resp.json();
+  } catch (e) {
+    throw new Error('Failed to create User');
+  }
+  if (jsonResp.error) {
+    throw new Error('Failed to create user');
+  }
+
+  if (!jsonResp.jwtToken) {
+    throw new Error('Failed to create user');
+  }
+  return jsonResp.jwtToken;
+}
+
+async function verifyToken() {
+  const token = getJwtToken();
+  if (!token) {
+    throw new Error('verification failed');
+  }
+
+  let jsonResp;
+  try {
+    const resp = await fetch(CREATE_USER_URL, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      jwtToken: token,
+    });
+    jsonResp = await resp.json();
+  } catch (e) {
+    throw new Error('Verification failed');
+  }
+
+  if (jsonResp.error) {
+    throw new Error('Verification failed');
+  }
+
+  if (!jsonResp.jwtToken) {
+    throw new Error('Verification failed');
+  }
+  storeJwtToken(jsonResp.jwtToken);
+}
+
+
+export {
+  storeJwtToken,
+  getJwtToken,
+  login,
+  createUser,
+  verifyToken,
+};
