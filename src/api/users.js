4219069de759f6d7e14e68da79ec82f2772bc3d4
William Post <wpost@hawaii.edu>
2020-03-13 10:47:34 -1000

    changed types of errors thrown to align with backend controller

--- src/api/users.js
+++ src/api/users.js
@@ -6,6 +6,32 @@ export const VERIFY_TOKEN_URL = `${API_ROOT_URL}/users/verify`;
 
 export const JWT_TOKEN = 'jwtToken';
 
+// todo: move Errors to be shared between client/server
+export const ERROR_TYPES = {
+  UNDEFINED: 'UNDEFINED',
+  MISSING_PARAMS: 'MISSING_PARAMS',
+  USER_EXISTS: 'USER_EXISTS',
+  PW_HASH_FAILED: 'PW_HASH_FAILED',
+  INSERT_QUERY_FAILED: 'INSERT_QUERY_FAILED',
+  TOKEN_SIGN_FAILED: 'TOKEN_SIGN_FAILED',
+  NO_USER_FOUND: 'NO_USER_FOUND',
+  PW_COMPARE_FAILED: 'PW_COMPARE_FAILED',
+  INVALID_TOKEN: 'INVALID_TOKEN',
+};
+
+export const ERRORS = {
+  [ERROR_TYPES.UNDEFINED]: new Error('An undefined server error has occurred.'),
+  [ERROR_TYPES.MISSING_PARAMS]: new Error('Missing params, make sure email and password is included in post body'),
+  [ERROR_TYPES.USER_EXISTS]: new Error('A user with the given email address already exists'),
+  [ERROR_TYPES.PW_HASH_FAILED]: new Error('Failed to hash password'),
+  [ERROR_TYPES.INSERT_QUERY_FAILED]: new Error('Failed to insert user into the database'),
+  [ERROR_TYPES.TOKEN_SIGN_FAILED]: new Error('Failed to tokenize user'),
+  [ERROR_TYPES.NO_USER_FOUND]: new Error('No user was found with given email and password'),
+  [ERROR_TYPES.PW_COMPARE_FAILED]: new Error('Failed to compare password hash'),
+  [ERROR_TYPES.INVALID_TOKEN]: new Error('Invalid Token'),
+};
+
+
 function storeJwtToken(token) {
   localStorage.setItem(JWT_TOKEN, token);
 }
@@ -29,15 +55,15 @@ async function login(email, password) {
     });
     jsonResp = await resp.json();
   } catch (e) {
-    throw new Error('Failed to login');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
 
   if (jsonResp.error) {
-    throw new Error('Failed to login');
+    throw ERRORS[ERROR_TYPES[jsonResp.error]];
   }
 
   if (!jsonResp.jwtToken) {
-    throw new Error('Failed to login');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
   return jsonResp.jwtToken;
 }
@@ -54,14 +80,14 @@ async function createUser(userObj) {
     });
     jsonResp = await resp.json();
   } catch (e) {
-    throw new Error('Failed to create User');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
   if (jsonResp.error) {
-    throw new Error('Failed to create user');
+    throw ERRORS[ERROR_TYPES[jsonResp]];
   }
 
   if (!jsonResp.jwtToken) {
-    throw new Error('Failed to create user');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
   return jsonResp.jwtToken;
 }
@@ -69,7 +95,7 @@ async function createUser(userObj) {
 async function verifyToken() {
   const token = getJwtToken();
   if (!token) {
-    throw new Error('verification failed');
+    throw ERRORS[ERROR_TYPES.INVALID_TOKEN];
   }
 
   let jsonResp;
@@ -85,19 +111,18 @@ async function verifyToken() {
     });
     jsonResp = await resp.json();
   } catch (e) {
-    throw new Error('Verification failed');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
 
   if (jsonResp.error) {
-    throw new Error('Verification failed');
+    throw ERRORS[ERROR_TYPES[jsonResp.error]];
   }
 
   if (!jsonResp.jwtToken) {
-    throw new Error('Verification failed');
+    throw ERRORS[ERROR_TYPES.UNDEFINED];
   }
   storeJwtToken(jsonResp.jwtToken);
 }
-
 
 export {
   storeJwtToken,
