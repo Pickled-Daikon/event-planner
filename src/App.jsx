c60d76f71342eec464819c2083c394b515d8ac86
William Post <wpost@hawaii.edu>
2020-03-12 20:10:57 -1000

    removed nav bar from rendering w/ all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import MainCalendarPage from './pages/MainCalendarPage';
-import NavBar from './components/NavBar';
 import Login from './pages/login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/dashboard" component={MainCalendarPage} />
