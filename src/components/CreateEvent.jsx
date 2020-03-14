00a9bb43b5ba366d7ced028d9d388a387f27c870
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2020-03-13 20:36:32 -1000

    added descriptions for error messages and logic for error

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -8,9 +8,79 @@ import {
 } from 'semantic-ui-calendar-react';
 import {createEvent} from "../api/events";
 
+const ALPHA_WC = /^[a-zA-Z]+$/;
+
+const MAX_NAME_LEN = 30;
+const MAX_DESCRIPTION_LEN = 120;
+
+
 const ERROR_MESSAGES = {
   SERVER_ERROR: 'We\'re sorry, the server is not responding right now',
+
+    NAME_OF_EVENT_IS_EMPTY: 'Failed to create event. Event must have a name.',
+    DATE_IS_EMPTY: 'Failed to create event. Please enter a date.',
+    TIME_START_IS_EMPTY: 'Failed to create event. Please provide the time start of event.',
+    TIME_END_IS_EMPTY: 'Failed to create event. Please provide the end time of event',
+
+    NAME_OF_EVENT_IS_TOO_LONG: `Failed to create event. Event must be no longer than ${MAX_NAME_LEN}`,
+    DESCRIPTION_IS_TOO_LONG: `Failed to create event. Description must be no longer than ${MAX_DESCRIPTION_LEN}`,
+
+    TIME_END_IS_BEFORE_TIME_START: 'Failed to create event. The event must not end before the start time',
+
+    INVALID_DATE_ENTERED: 'Failed to create event. Please enter a valid date',
+    INVALID_TIME_ENTERED: 'Failed to create event. Please enter a valid date',
+
 };
+
+/**
+ *
+ * @param {{name, description, location, date, timeStart, timeEnd}} fieldValues
+ */
+function fieldErrorCheck(fieldValues) {
+    // renamed consts since consistent names is causing problems w/ react hooks
+    const eName = fieldValues.name;
+    const desc = fieldValues.description;
+    const eDate = fieldValues.date;
+    const loc = fieldValues.location;
+    const tStart = fieldValues.timeStart;
+    const tEnd = fieldValues.timeEnd;
+
+    /* ****** no empty fields ****** */
+    if (eName === '' || !eName) {
+        return ERROR_MESSAGES.NAME_OF_EVENT_IS_EMPTY;
+    }
+    if (eDate === '' || !eDate) {
+        return ERROR_MESSAGES.DATE_IS_EMPTY;
+    }
+    if (tStart === '' || !tStart) {
+        return ERROR_MESSAGES.TIME_START_IS_EMPTY;
+    }    if (tEnd === '' || !tEnd) {
+        return ERROR_MESSAGES.TIME_END_IS_EMPTY;
+    }
+
+    /* ******* EXCEEDED CHAR LIMIT ******* */
+    if (eName.length > MAX_NAME_LEN) {
+        return ERROR_MESSAGES.NAME_OF_EVENT_IS_TOO_LONG;
+    }
+    if (desc.length > MAX_DESCRIPTION_LEN) {
+        return ERROR_MESSAGES.DESCRIPTION_IS_TOO_LONG;
+    }
+
+
+    /* ******* TIME END BEFORE TIME START ERROR ******* */
+    if (tStart.slice(0,2) < tEnd.slice(0,2)) {
+        return ERROR_MESSAGES.TIME_END_IS_BEFORE_TIME_START;
+    }
+
+    /* ******** CHECKING DATE AND TIME FOR INVALID CHARACTERS ******* */
+    if (eDate.match(ALPHA_WC)) {
+        return ERROR_MESSAGES.INVALID_DATE_ENTERED;
+    }
+    if (tStart.match(ALPHA_WC) || tEnd.match(ALPHA_WC)) {
+        return ERROR_MESSAGES.INVALID_TIME_ENTERED;
+    }
+    return null;
+}
 
 export const CREATE_EVENT_STATUSES = {
     DEFAULT: 0,
@@ -42,6 +112,7 @@ class CreateEvent extends React.Component {
       this.setState(({errorMsg: msg}));
     };
 
+
     onSubmit = () => {
         const {
           name,
@@ -51,6 +122,7 @@ class CreateEvent extends React.Component {
           startTime,
           endTime,
         } = this.state;
+
 
       const dateArr = date.split('-');
       const newDate = [dateArr[1], dateArr[0], dateArr[2]].join('-');
